0f4663679c299b84880b85ea691e191f07d086e1
Iqra-Hayder <iqrahayder001@gmail.com>
2024-11-30 16:10:49 +0500

    Address class added

--- Main.java
+++ Main.java
@@ -1,5 +1,47 @@
 package OOP_project;
 
+
+class Address{
+ private String city;
+ private int zipCode;
+////// Constructors
+Address(){}
+Address(String city, int zipCode){
+    this.city = city;
+    this.zipCode = zipCode;
+}
+/////// setters
+public void setCity(String city){
+   this.city = city;
+}
+public void setZipCode(int zipCode){
+  this.zipCode = zipCode;
+}
+/////// getters
+public String getCity(){
+  return city;
+}
+public int getZipCode(){
+  return zipCode;
+}
+
+@Override
+public String toString(){
+  return "City: " + city + ", Zip Code: " + zipCode ;
+}
+
+}
+
 public class Main {
+  public static void main(String[] args) {
+  Address address = new Address("islamabad", 455000);
+  System.out.println(address.getCity());
+  address.setCity("lahore");
+  System.out.println(address.getZipCode());
+  address.setZipCode(3456767);
+  System.out.println(address.toString());
+  }
+  
+
   
 }
