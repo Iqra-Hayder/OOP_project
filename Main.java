122cad26e46417116d05b8e8554d6e4665d195c5
Kacho Ayoub <kachoayoubkhan03@gmail.com>
2024-11-30 16:11:25 +0500

    Person class added

--- Main.java
+++ Main.java
@@ -1,5 +1,46 @@
 package OOP_project;
-
+class Person{
+  protected String name;
+  protected String email;
+  protected String dateOfBirth;
+//constructors
+  Person(){
+  }
+  Person(String nam, String email, String date){
+    this.name = nam;
+    this.email = email;
+    this.dateOfBirth = date;
+  }
+//setters
+  public void setName(String nam){
+    this.name = nam;
+  }
+  public void setEmail(String email){
+    this.email = email;
+  }
+  public void setDateOfBirth(String date){
+    this.dateOfBirth = date;
+  }
+//getters
+  public String getName(){
+    return this.name;
+  }
+  public String getEmail(){
+    return this.email;
+  }
+  public String getDateOfBirth(){
+    return this.dateOfBirth;
+  }
+  @Override
+  public String toString(){
+    return "Name: "+this.name+"\nEamil: "+this.email+"\nDate Of Birth: "+this.dateOfBirth;
+  }
+}
 public class Main {
+    public static void main(String[] args) {
+        Person p = new Person("abc","abc@gamail","11/11/2003");
+        System.out.println(p.toString());   
+    }
+  
   
 }
